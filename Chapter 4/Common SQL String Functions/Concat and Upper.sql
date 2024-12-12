4b40da96775148b24e85d39745fb1490880c20a9
tbcheuvront <tristenhanson89@gmail.com>
2024-12-12 18:40:37 +0000

    modified:   .vscode/settings.json modified:   Chapter 4/Common SQL String Functions/Concat and Upper.sql

--- Chapter 4/Common SQL String Functions/Concat and Upper.sql
+++ Chapter 4/Common SQL String Functions/Concat and Upper.sql
@@ -1,13 +1,15 @@
-SELECT UPPER(
-    CONCAT(
-      Address,
+SELECT 
+  UPPER(CONCAT
+      (Address,
       ' ',
       City,
-      ', ',
+      ' ',
       State,
       ' ',
-      Zipcode
-    )
-  ) AS NEW_ADDRESS
-from Customer
-LIMIT 10
+      Zipcode)) AS NEW_ADDRESS
+-- Note that || does not work to concatenate in this program. You must
+-- use the concat() function.
+FROM
+ Customer
+LIMIT
+  10
